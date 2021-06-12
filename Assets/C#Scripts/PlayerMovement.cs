a95a6c3421d84435e83e45e33bf1f425d4ef4a61
SomeonesShade <jomil.paulo@gmail.com>
2021-06-12 20:27:10 +0800

    Player v0.5
    Added PlayerScene and Some C#Scripts

---
+++ Assets/C#Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public bool OnGround;
+    public float speed;
+    public float jumpForce;
+    public bool jumpCooldown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        jumpCooldown = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Movement();
+    }
+    void Movement()
+    {
+        
+        rb.velocity = new Vector2(speed * Input.GetAxisRaw("Horizontal") * Time.fixedDeltaTime,rb.velocity.y);
+        if (OnGround && jumpCooldown)
+        {
+            rb.AddForce(new Vector2(0,Input.GetAxisRaw("Jump") * jumpForce), ForceMode2D.Impulse);
+        }
+    }
+    IEnumerator CoolDown()
+    {
+        jumpCooldown = false;
+        yield return new WaitForSeconds(0.1f);
+        jumpCooldown = true;
+    }
+}
